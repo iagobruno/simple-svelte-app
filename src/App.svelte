669f3827daf31e9e26b8df497dc000aac84a656b
Iago Bruno <iagobruno.dev@gmail.com>
2020-12-16 15:29:17 -0300

    feat: Scroll to top on route change

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,28 @@
 <script>
+  import { onMount } from 'svelte'
   import { Router, Route } from 'svelte-routing'
 
   import Home from './pages/Home.svelte'
   import Profile from './pages/Profile.svelte'
   import Repo from './pages/Repo.svelte'
   import Error from './pages/Error.svelte'
+
+  function onRouteChange (newPath) {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+    console.log('Route changed to:', newPath)
+  }
+
+  onMount(async () => {
+    // Stolen from https://github.com/EmilTholin/svelte-routing/issues/132
+    history.pushState = new Proxy(history.pushState, {
+      apply (target, thisArg, argumentsList) {
+        Reflect.apply(target, thisArg, argumentsList)
+
+        const newPath = argumentsList[2]
+        onRouteChange(newPath)
+      }
+    })
+  })
 </script>
 
 <Router>
